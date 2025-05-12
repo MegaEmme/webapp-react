889c445c70be97867daa5b5a489cdbb9414d7fb0
MegaEmme <mikmusco@gmail.com>
2025-05-12 16:29:32 +0200

    feat: bonus

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,10 +7,10 @@ const MovieCard = ({ data }) => {
     return (
         <>
             <div className="card mb-5 bg-tertiary">
-                <img src={imagePath} alt={title} className="w-50 mx-auto p-2" />
+                <img src={imagePath} alt={title} className="w-50 mx-auto p-2 mt-2" />
                 <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p>Voto: <strong>{review_vote}</strong></p>
+                    <h5 className="card-title text-center">{title}</h5>
+                    <p className="text-end">Voto: <strong>{review_vote}</strong></p>
                     <p>Regia: <strong>{director}</strong></p>
                     <p>Genere: <strong>{genre}</strong></p>
                     <p>Anno uscita: <strong>{release_year}</strong></p>
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -4,26 +4,46 @@ import axios from "axios";
 
 const MoviesPage = () => {
     const [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState('');
 
     function getMovies() {
-        axios.get('http://localhost:3000/api/movies')
+        axios.get('http://localhost:3000/api/movies', {
+            params: {
+                search
+            }
+        })
             .then(response => {
-                console.log(response.data);
                 setMovies(response.data);
             })
             .catch(err => console.log(err));
+    }
+
+    function searchMovies(e) {
+        e.preventDefault();
+        getMovies();
     }
 
     useEffect(getMovies, []);
 
     return (
         <>
-            <h1>Films</h1>
+            <h1 className="text-center mb-4">Films</h1>
             <section>
-                <h2>Best Movies Ever</h2>
-                <div className="row">
+                <div className="d-flex justify-content-between">
+                    <h2>Lista film:</h2>
+                    <form className="row g-1" onSubmit={searchMovies}>
+                        <div className="col-auto">
+                            <label className="visually-hidden">Ricerca film</label>
+                            <input type="text" className="form-control" placeholder="Ricerca film..." value={search} onChange={(e) => setSearch(e.target.value)} />
+                        </div>
+                        <div className="col-auto">
+                            <button type="submit" className="btn btn-primary mb-3">Cerca</button>
+                        </div>
+                    </form>
+                </div>
+                <div className="row mt-auto h-100">
                     {movies.length ? movies.map(movie => (
-                        <div className="col-12 col-md-4 col-lg-3" key={movie.id}>
+                        <div className="col-12 col-md-6 col-lg-4 col-xl-3" key={movie.id}>
                             <MovieCard data={movie} />
                         </div>
                     )) : <div>Film non trovato </div>}

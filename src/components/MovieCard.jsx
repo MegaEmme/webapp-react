721da317164bdb5644cec5b52f5db5e98e3820fc
MegaEmme <mikmusco@gmail.com>
2025-05-12 15:41:44 +0200

    feat: implement movie fetching and display in MoviesPage, enhance MovieCard with details

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,21 @@
-const MovieCard = () => {
+import { Link } from "react-router-dom";
+
+const MovieCard = ({ data }) => {
+
+    const { id, title, director, genre, release_year, abstract, review_vote, imagePath } = data;
+
     return (
         <>
-            <div className="card">
-                image
+            <div className="card mb-5 bg-tertiary">
+                <img src={imagePath} alt={title} className="w-50 mx-auto p-2" />
                 <div className="card-body">
-                    <h5 className="card-title">Titolo</h5>
-                    <p>Author: <strong>Autore</strong></p>
-                    <p className="card-text">Descrizione</p>
-                    <a href="#" className="btn btn-primary">Vai a </a>
+                    <h5 className="card-title">{title}</h5>
+                    <p>Voto: <strong>{review_vote}</strong></p>
+                    <p>Regia: <strong>{director}</strong></p>
+                    <p>Genere: <strong>{genre}</strong></p>
+                    <p>Anno uscita: <strong>{release_year}</strong></p>
+                    <p className="card-text">{abstract}</p>
+                    <Link to={`/movies/${id}`} className="btn btn-primary">Scheda Film</Link>
                 </div>
             </div>
         </>
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
+import axios from "axios";
 
 const MoviesPage = () => {
+    const [movies, setMovies] = useState([]);
+
+    function getMovies() {
+        axios.get('http://localhost:3000/api/movies')
+            .then(response => {
+                console.log(response.data);
+                setMovies(response.data);
+            })
+            .catch(err => console.log(err));
+    }
+
+    useEffect(getMovies, []);
+
     return (
         <>
             <h1>Films</h1>
             <section>
                 <h2>Best Movies Ever</h2>
                 <div className="row">
-                    <div className="col-12 col-md-4">
-                        <MovieCard />
-                    </div>
+                    {movies.length ? movies.map(movie => (
+                        <div className="col-12 col-md-4 col-lg-3" key={movie.id}>
+                            <MovieCard data={movie} />
+                        </div>
+                    )) : <div>Film non trovato </div>}
                 </div>
             </section>
         </>
